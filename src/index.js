7377e2f5da0f5c50ebda0781137dda97d9b1623b
alexgamadev <alexgama.97@outlook.com>
2021-03-20 21:35:30 +0000

    Removed links array and updated resolvers to use prisma

--- src/index.js
+++ src/index.js
@@ -6,53 +6,28 @@ import Prisma from '@prisma/client'
 const { PrismaClient } = Prisma
 const prisma = new PrismaClient();
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-
 const typeDefs = fs.readFileSync(
     new URL('schema.graphql', import.meta.url),
     'utf8',
 );
 
-let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `Placeholder info`,
-        feed: () => links,
-        link: (parent, args) => {
-            return links.find(link => args.id === link.id);
-        }
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-            links.push(link);
+        post: (parent, args, context) => {
+            const link = context.prisma.link.create({
+                data: {
+                    description: args.description,
+                    url: args.url,
+                }
+            });
             return link;
         },
-        updateLink: (parent, args) => {
-            const linkIndex = links.findIndex(link => args.id === link.id);
-            const link = {...links[linkIndex]};
-            link.url = args.url ?? link.url;
-            link.description = args.description ?? link.description;
-            links[linkIndex] = link;
-            return links[linkIndex];
-        },
-        deleteLink: (parent, args) => {
-            const linkIndex = links.findIndex(link => args.id === link.id);
-            if(linkIndex !== -1) {
-                const link = links.splice(linkIndex, 1);
-                console.log(link);
-                return link[0];
-            }
-
-        }
     }
 };
 
