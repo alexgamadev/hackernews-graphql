202e1a9ceddd448de82e7149e32c5d95a3c9c49a
alexgamadev <alexgama.97@outlook.com>
2021-03-20 20:15:29 +0000

    Added feed query field

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import { ApolloServer } from 'apollo-server';
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `;
 
 const resolvers = {
     Query: {
         info: () => `Placeholder info`,
+        feed: () => links,
     }
 };
 
